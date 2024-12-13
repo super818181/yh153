995333afcc0d087f06b5a66684dbe02d4a1d60d2
super818181 <super818181@outlook.com>
2024-12-13 14:05:30 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,13 @@
+/*-------config start-------*/
+var _wip = '43.138.162.153';  
+var _port = '1730';  
+/*---------config end---------*/
+
+var BaseUrl = 'http://' + _wip+':'+_port;
+var ResourceUrl = '/';
+var Suffix = '.html';
+var _isTx = 1;
+var _wport = '10001';
+
+
+/*document.write('<script type="text/javascript" src="http://' + _wip + ':1999/cwoss.js?v=' + _tt + '"> <\/script>');*/
